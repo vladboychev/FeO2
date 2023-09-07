eb4b218e824d7bc294a11ab643eb2319f4d2c393
Vlad Boychev <vladboychev@gmail.com>
2023-09-07 19:11:20 +0300

    weight on mars

---
+++ weight_on_mars/target/debug/deps/weight_on_mars-f2e7b9f8ebac9301.d
@@ -0,0 +1,5 @@
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/deps/weight_on_mars-f2e7b9f8ebac9301: src/main.rs
+
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/deps/weight_on_mars-f2e7b9f8ebac9301.d: src/main.rs
+
+src/main.rs:
---
+++ weight_on_mars/target/debug/weight_on_mars.d
@@ -0,0 +1,1 @@
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/weight_on_mars: /home/vlad/Code/FeO2/weight_on_mars/src/main.rs
