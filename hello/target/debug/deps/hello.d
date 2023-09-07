1668c722c54ce51fc0fee458599bb24c09365ac1
Vlad Boychev <vladboychev@gmail.com>
2023-09-07 10:39:07 +0300

    hello

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+D:\CODE\Lrn\FeO2\hello\target\debug\deps\hello.exe: src\main.rs
+
+D:\CODE\Lrn\FeO2\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+D:\CODE\Lrn\FeO2\hello\target\debug\hello.exe: D:\CODE\Lrn\FeO2\hello\src\main.rs
