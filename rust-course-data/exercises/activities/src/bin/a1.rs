38b61029e9ccfcac60f3b6630ce849a8a0534108
Vlad Boychev <vladboychev@gmail.com>
2023-09-07 11:52:58 +0300

    temp walkthrough

--- rust-course-data/exercises/activities/src/bin/a1.rs
+++ rust-course-data/exercises/activities/src/bin/a1.rs
@@ -5,7 +5,20 @@
 //
 // Notes:
 // * Use a function to display your first name
+fn first_name() {
+    println!("Vladimir");
+}
 // * Use a function to display your last name
+fn last_name() {
+    println!("Boychev");
+}
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+
+    first_name();
+    last_name();
+
+    println!();
+
+}
