c0d640e826f95dd3f5bd7f47334d3dd36ab9f3ee
Vlad Boychev <vladboychev@gmail.com>
2023-09-07 10:33:15 +0300

    initial commit

---
+++ rocket-app/src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use] extern crate rocket;
+
+use rocket::serde::json::{Value, json};
+
+#[get("/")]
+fn hello() -> Value {
+    json!("Hello, world!")
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![hello])
+        .launch()
+        .await;
+}
