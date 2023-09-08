366ef4ca30bfde65eaa289910fe6a12d170119d8
Vlad Boychev <vladboychev@gmail.com>
2023-09-08 19:20:20 +0300

    landed on mars

---
+++ weight_on_mars/target/debug/deps/weight_on_mars-4b91e9f53a9dc4d1.d
@@ -0,0 +1,5 @@
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/deps/weight_on_mars-4b91e9f53a9dc4d1.rmeta: src/main.rs
+
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/deps/weight_on_mars-4b91e9f53a9dc4d1.d: src/main.rs
+
+src/main.rs:
---
+++ weight_on_mars/target/debug/deps/weight_on_mars-f77193cfa05aaed9.d
@@ -0,0 +1,5 @@
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/deps/weight_on_mars-f77193cfa05aaed9.rmeta: src/main.rs
+
+/home/vlad/Code/FeO2/weight_on_mars/target/debug/deps/weight_on_mars-f77193cfa05aaed9.d: src/main.rs
+
+src/main.rs:
